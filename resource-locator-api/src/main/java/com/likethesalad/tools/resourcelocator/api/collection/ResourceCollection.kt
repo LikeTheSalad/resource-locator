6aecc279db5c39c531b14aba793bf175ebbf68c5
Cesar <likethesalad@gmail.com>
2021-07-05 15:20:02 +0100

    Removed ResourceCollectionScope

--- resource-locator-api/src/main/java/com/likethesalad/tools/resourcelocator/api/collection/ResourceCollection.kt
+++ resource-locator-api/src/main/java/com/likethesalad/tools/resourcelocator/api/collection/ResourceCollection.kt
@@ -1,9 +1,10 @@
 package com.likethesalad.tools.resourcelocator.api.collection
 
 import com.likethesalad.tools.resourcelocator.api.Resource
+import com.likethesalad.tools.resourcelocator.api.ResourceScope
 
 interface ResourceCollection<out T : Resource<out Any>> {
     fun getResources(): List<T>
     fun getSource(): Any
-    fun getScope(): ResourceCollectionScope
+    fun getScope(): ResourceScope
 }
--- resource-locator-api/src/main/java/com/likethesalad/tools/resourcelocator/api/collection/ResourceCollectionScope.kt
+++
@@ -1,5 +0,0 @@
-package com.likethesalad.tools.resourcelocator.api.collection
-
-import com.likethesalad.tools.resourcelocator.api.common.Named
-
-interface ResourceCollectionScope : Named

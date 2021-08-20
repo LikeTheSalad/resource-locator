82efd69a7667f8c82417f256ef53e5e1a78081b7
Cesar <likethesalad@gmail.com>
2021-08-20 07:05:00 +0100

    Created Android resource serializer

---
+++ android-resource-serializer/src/main/java/com/likethesalad/resource/serializer/android/AndroidResourceSerializer.kt
@@ -0,0 +1,14 @@
+package com.likethesalad.resource.serializer.android
+
+import com.likethesalad.tools.resource.api.Resource
+import com.likethesalad.tools.resource.serializer.ResourceSerializer
+
+class AndroidResourceSerializer : ResourceSerializer {
+    override fun serialize(resource: Resource): String {
+        TODO("Not yet implemented")
+    }
+
+    override fun deserialize(string: String): Resource {
+        TODO("Not yet implemented")
+    }
+}

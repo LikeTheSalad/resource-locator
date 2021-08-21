a68b017f1a6694d11581214c6bb51d98493df8ea
Cesar <likethesalad@gmail.com>
2021-08-21 10:49:34 +0100

    Added Androis resource type Unknown

--- android-resource-api/src/main/java/com/likethesalad/tools/resource/api/android/data/AndroidResourceType.kt
+++ android-resource-api/src/main/java/com/likethesalad/tools/resource/api/android/data/AndroidResourceType.kt
@@ -5,6 +5,7 @@ import com.likethesalad.tools.resource.api.data.ResourceType
 sealed class AndroidResourceType(private val name: String) : ResourceType {
     object StringType : AndroidResourceType("string")
     object IntegerType : AndroidResourceType("integer")
+    class Unknown(name: String) : AndroidResourceType(name)
 
     override fun getName(): String = name
 

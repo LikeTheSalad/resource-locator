570b887b513736114a0707c045ace855b92de1ad
Cesar <likethesalad@gmail.com>
2021-08-25 07:23:45 +0100

    Creating Gradle Action observer

--- android-resource-locator/src/main/java/com/likethesalad/tools/resource/locator/android/extension/ResourceLocatorExtension.kt
+++ android-resource-locator/src/main/java/com/likethesalad/tools/resource/locator/android/extension/ResourceLocatorExtension.kt
@@ -1,13 +1,15 @@
 package com.likethesalad.tools.resource.locator.android.extension
 
 import com.likethesalad.tools.resource.locator.android.extension.observer.ResourceLocatorTaskContainer
+import com.likethesalad.tools.resource.locator.android.extension.observer.ResourceLocatorTaskPublisher
+import com.likethesalad.tools.resource.locator.android.extension.observer.observers.ActionResourceLocatorTaskObserver
 import org.gradle.api.Action
 import java.io.File
 
-open class ResourceLocatorExtension {
+open class ResourceLocatorExtension(private val taskPublisher: ResourceLocatorTaskPublisher) {
 
     fun onResourceLocatorTaskCreated(action: Action<ResourceLocatorTaskContainer>) {
-
+        taskPublisher.register(ActionResourceLocatorTaskObserver(action))
     }
 
     fun getResourcesFromDir(directory: File): LanguageResourceFinder {
---
+++ android-resource-locator/src/main/java/com/likethesalad/tools/resource/locator/android/extension/observer/observers/ActionResourceLocatorTaskObserver.kt
@@ -0,0 +1,26 @@
+package com.likethesalad.tools.resource.locator.android.extension.observer.observers
+
+import com.likethesalad.tools.resource.locator.android.extension.observer.ResourceLocatorTaskContainer
+import com.likethesalad.tools.resource.locator.android.extension.observer.ResourceLocatorTaskObserver
+import org.gradle.api.Action
+
+class ActionResourceLocatorTaskObserver(private val action: Action<ResourceLocatorTaskContainer>) :
+    ResourceLocatorTaskObserver {
+
+    override fun notify(taskContainer: ResourceLocatorTaskContainer) {
+        action.execute(taskContainer)
+    }
+
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (other !is ActionResourceLocatorTaskObserver) return false
+
+        if (action != other.action) return false
+
+        return true
+    }
+
+    override fun hashCode(): Int {
+        return action.hashCode()
+    }
+}

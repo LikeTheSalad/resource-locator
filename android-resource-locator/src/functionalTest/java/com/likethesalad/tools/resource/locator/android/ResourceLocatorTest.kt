446909993950d5133fd19ac98ac413e08bb80556
Cesar <likethesalad@gmail.com>
2021-08-28 12:28:10 +0100

    Configuring new functionalTest sourceSets

---
+++ android-resource-locator/src/functionalTest/java/com/likethesalad/tools/resource/locator/android/ResourceLocatorTest.kt
@@ -0,0 +1,20 @@
+package com.likethesalad.tools.resource.locator.android
+
+import com.likethesalad.tools.functional.testing.AndroidProjectTest
+import com.likethesalad.tools.functional.testing.data.JarParameters
+
+class ResourceLocatorTest: AndroidProjectTest() {
+
+    override fun getAndroidBuildPluginVersion(): String {
+        TODO("Not yet implemented")
+    }
+
+    override fun getGradleVersion(): String {
+        TODO("Not yet implemented")
+    }
+
+    override fun getPluginJarParameters(): JarParameters {
+        TODO("Not yet implemented")
+    }
+
+}

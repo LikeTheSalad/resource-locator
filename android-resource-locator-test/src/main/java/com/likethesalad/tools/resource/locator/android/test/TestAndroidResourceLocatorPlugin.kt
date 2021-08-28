fb27e0e681d389aeebd5519d7c920ff87309474f
Cesar <likethesalad@gmail.com>
2021-08-28 20:58:21 +0100

    Created test xml resource extractor

--- android-resource-locator-test/src/main/java/com/likethesalad/tools/resource/locator/android/test/TestAndroidResourceLocatorPlugin.kt
+++ android-resource-locator-test/src/main/java/com/likethesalad/tools/resource/locator/android/test/TestAndroidResourceLocatorPlugin.kt
@@ -1,17 +1,25 @@
 package com.likethesalad.tools.resource.locator.android.test
 
+import com.likethesalad.tools.android.plugin.AndroidExtension
 import com.likethesalad.tools.resource.collector.ResourceCollector
+import com.likethesalad.tools.resource.collector.android.AndroidResourceCollector
 import com.likethesalad.tools.resource.collector.android.data.variant.VariantTree
 import com.likethesalad.tools.resource.locator.android.AndroidResourceLocatorPlugin
+import com.likethesalad.tools.resource.locator.android.test.extractor.TestXmlResourceExtractor
 
 class TestAndroidResourceLocatorPlugin : AndroidResourceLocatorPlugin() {
 
-    override fun getLocatorId(): String {
-        TODO("Not yet implemented")
+    private val resourceExtractor by lazy {
+        TestXmlResourceExtractor()
     }
 
-    override fun getResourceCollector(variantTree: VariantTree): ResourceCollector {
-        TODO("Not yet implemented")
+    override fun getLocatorId(): String = "test"
+
+    override fun getResourceCollector(
+        androidExtension: AndroidExtension,
+        variantTree: VariantTree
+    ): ResourceCollector {
+        return AndroidResourceCollector.newInstance(androidExtension, variantTree, resourceExtractor)
     }
 
 }

31da9cf1c07d6474bc91431fd6a346e2b1f0f0ba
Cesar <likethesalad@gmail.com>
2021-09-27 07:03:10 +0100

    Created StringXmlResourceExtractor and implementing it

--- string-android-resource-locator/src/main/java/com/likethesalad/android/string/resources/locator/StringResourceLocatorPlugin.kt
+++ string-android-resource-locator/src/main/java/com/likethesalad/android/string/resources/locator/StringResourceLocatorPlugin.kt
@@ -1,16 +1,20 @@
 package com.likethesalad.android.string.resources.locator
 
+import com.likethesalad.android.string.resources.locator.extractor.StringXmlResourceExtractor
 import com.likethesalad.tools.resource.collector.ResourceCollector
+import com.likethesalad.tools.resource.collector.android.AndroidResourceCollector
 import com.likethesalad.tools.resource.collector.android.data.variant.VariantTree
 import com.likethesalad.tools.resource.locator.android.AndroidResourceLocatorPlugin
 
 class StringResourceLocatorPlugin : AndroidResourceLocatorPlugin() {
 
-    override fun getLocatorId(): String {
-        TODO("Not yet implemented")
+    private val extractor by lazy {
+        StringXmlResourceExtractor()
     }
 
+    override fun getLocatorId(): String = "string"
+
     override fun getResourceCollector(variantTree: VariantTree): ResourceCollector {
-        TODO("Not yet implemented")
+        return AndroidResourceCollector.newInstance(getAndroidExtension(), variantTree, extractor)
     }
 }
